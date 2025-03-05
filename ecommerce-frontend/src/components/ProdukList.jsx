db3f17235bb2030b1b39aa001d55ad68a89d74ff
unknown <10231067@student.itk.ac.id>
2025-03-05 11:30:35 +0800

    1 gang

---
+++ ecommerce-frontend/src/components/ProdukList.jsx
@@ -0,0 +1,23 @@
+// src/components/ProdukList.jsx
+import React, { useEffect, useState } from 'react';
+function ProdukList() {
+const [produk, setProduk] = useState([]);
+useEffect(() => {
+// Data statis sementara
+setProduk([
+{ id: 1, nama: 'Produk N' },
+{ id: 2, nama: 'Produk R' },
+]);
+}, []);
+return (
+<div>
+<h2>Daftar Produk</h2>
+<ul>
+{produk.map((item) => (
+<li key={item.id}>{item.nama}</li>
+))}
+</ul>
+</div>
+);
+}
+export default ProdukList;
