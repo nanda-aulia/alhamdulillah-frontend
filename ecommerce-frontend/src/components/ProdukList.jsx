4a6eb84f0f1623908dc154c2c4ef3cf9a21c67a9
unknown <10231067@student.itk.ac.id>
2025-03-13 10:49:20 +0800

    push to nanda

--- ecommerce-frontend/src/components/ProdukList.jsx
+++ ecommerce-frontend/src/components/ProdukList.jsx
@@ -1,23 +1,32 @@
 // src/components/ProdukList.jsx
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function ProdukList() {
-const [produk, setProduk] = useState([]);
-useEffect(() => {
-// Data statis sementara
-setProduk([
-{ id: 1, nama: 'Produk N' },
-{ id: 2, nama: 'Produk R' },
-]);
-}, []);
-return (
-<div>
-<h2>Daftar Produk</h2>
-<ul>
-{produk.map((item) => (
-<li key={item.id}>{item.nama}</li>
-))}
-</ul>
-</div>
-);
+  const [produk, setProduk] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/produk')
+      .then((response) => {
+        setProduk(response.data);
+      })
+      .catch((error) => {
+        console.error('Terjadi error:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Daftar Produk (From Database)</h2>
+      <ul>
+        {produk.map((item) => (
+          <li key={item.id}>
+            {item.nama} - Rp{item.harga}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default ProdukList;
