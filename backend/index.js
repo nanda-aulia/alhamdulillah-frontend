453429ac1ec477f687b693e28aab7ba0f894844a
unknown <10231079@student.itk.ac.id>
2025-03-13 09:38:02 +0800

    Add basic Express.js setup and routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-// index.js
+// index.js (gabungan)
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db');
 const app = express();
 const PORT = 3001;
 app.use(cors());
@@ -12,6 +13,31 @@ app.post('/data', (req, res) => {
 const { nama } = req.body;
 res.send(`Data diterima: ${nama}`);
 });
-app.listen(PORT, () => {
-console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+// CREATE
+app.post('/produk', async (req, res) => {
+const { nama, harga } = req.body;
+try {
+const newProduk = await pool.query(
+'INSERT INTO produk (nama, harga) VALUES ($1, $2) RETURNING *',
+[nama, harga]
+);
+res.json(newProduk.rows[0]);
+} catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+    }
+    });
+    // READ
+    app.get('/produk', async (req, res) => {
+    try {
+    const allProduk = await pool.query('SELECT * FROM produk');
+    res.json(allProduk.rows);
+    } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+    }
+    });
+    app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+    });
+    
